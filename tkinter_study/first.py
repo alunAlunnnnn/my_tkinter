9cc8436484a44467e9cac2a129d8e603d5d89448
alun <517308447@qq.com>
2021-08-22 23:34:51 +0800

    init repo

---
+++ tkinter_study/first.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+
+class MainForm:
+    def __init__(self):
+        # 窗体实例
+        root = tk.Tk()
+        root.mainloop()
+
+
+def main():
+    MainForm()
+
+
+if __name__ == '__main__':
+    main()
